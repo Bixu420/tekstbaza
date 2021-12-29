adfe22afde95d19d60112e8837ca56ad0c0935ad
Xawery <ksawerynw@gmail.com>
2021-12-29 23:06:02 +0100

    zmiany, nie udalo sie tylko ostatniego podpunktu zrobic

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 print("podaj ilosc paczek")
-ilosc=int(input())
+import sys
 waga=0 #ogolna waga paczki
+ilosc = int(sys.argv[1])
 ciezar=0 # ciezar pakunku
 lacznawaga=0
 nadciezar=0  #nadwaga
-nrpaczki=0
-for i in range (ilosc):
+nrpaczki=20 #paczka z najwieksza ilosc pustych kg
+samnwmco=0
+i=0
+while i<ilosc:
     print("podaj wage pakunku")
     ciezar=float(input())
-    if ciezar==0 or waga==20:
-        ilosc=ilosc+1
-        lacznawaga=waga+lacznawaga
+
+    if ciezar==0 or waga+ciezar==20:
+        if waga<nrpaczki:
+            nrpaczki=i
+
+
+        i=i+1
+
         waga=0
+        print("nastepna paczka")
         continue
-    if ciezar >10 or ciezar<1:
+    if ciezar>10 or ciezar<1:
         print("podales za duza/mala waga pakunku")
         continue
+    else:
+        lacznawaga=lacznawaga+ciezar
 
     if waga + ciezar > 20:
+        if waga<nrpaczki:
+            nrpaczki=i
         waga=0
         nadciezar=ciezar
-        lacznawaga = waga + lacznawaga
-        nrpaczki=nrpaczki+1
-        ilosc=ilosc+1
+
+
+        i=i+1
         continue
     waga= ciezar + waga + nadciezar
+    print(waga)
+
+    print("nr paczki", i+1)
 
     nadciezar=0
-print(lacznawaga)
+print("liczba paczek wyslanych to", ilosc)
+print("liczba kg wyslanych to", lacznawaga)
+print("liczba pustych kg=", ilosc*20-lacznawaga)
+print(nrpaczki+1)
+
