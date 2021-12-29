73de90f1bd11266f0aa9a841e464cca2afd208e2
Xawery <ksawerynw@gmail.com>
2021-12-29 22:08:04 +0100

    nie wiem dlaczego petla sie wczesnie konczy

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-paczka=20
-waga=0
 print("podaj ilosc paczek")
-iloscpaczek=int(input())
-max=iloscpaczek*paczka
-suma=0
-calkowitawaga=0
+ilosc=int(input())
+waga=0 #ogolna waga paczki
+ciezar=0 # ciezar pakunku
+lacznawaga=0
+nadciezar=0  #nadwaga
+nrpaczki=0
+for i in range (ilosc):
+    print("podaj wage pakunku")
+    ciezar=float(input())
+    if ciezar==0 or waga==20:
+        ilosc=ilosc+1
+        lacznawaga=waga+lacznawaga
+        waga=0
+        continue
+    if ciezar >10 or ciezar<1:
+        print("podales za duza/mala waga pakunku")
+        continue
 
+    if waga + ciezar > 20:
+        waga=0
+        nadciezar=ciezar
+        lacznawaga = waga + lacznawaga
+        nrpaczki=nrpaczki+1
+        ilosc=ilosc+1
+        continue
+    waga= ciezar + waga + nadciezar
 
-
-for i in range(1,iloscpaczek+1, 1):
-
-    print("paczka nr", i)
-
-    for nr in range(paczka):
-
-
-        print("podaj wage")
-        a = int(input())
-        if a>10 or a<0:
-            print("za duzy ciezar")
-            continue
-        waga=waga + a
-
-
-        if a == 0 or waga>=20:
-            print("zamykamy paczke i pakujemy kolejna")
-            break
-    calkowitawaga=waga+calkowitawaga
-    waga = 0
-print("liczba wyslanych paczek to", iloscpaczek)
-print ("wyslane kilogramy=", calkowitawaga)
-print ("puste kilogramy=", max-calkowitawaga)
+    nadciezar=0
+print(lacznawaga)
