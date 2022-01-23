65715c6a9f8a194d7a10548614ec7ec3230b57ca
Xawery <ksawerynw@gmail.com>
2022-01-23 13:25:48 +0100

    lool

--- ksiegowy/main2.py
+++ ksiegowy/main2.py
@@ -8,6 +8,10 @@ class Magazyn:
         self.magazyn={}
         self.historia=[]
         self.calkowite=0
+        self.baza=baza
+
+
+
 
     def zakup(self,nazwa, ilosc, cena):
         saldo=ilosc*cena
@@ -31,29 +35,53 @@ class Magazyn:
         self.historia.append(["saldo", komentarz, calkowite])
         return True
 
-    def odczyt(self):
+    def lista(self):
         with open (self.sciezka, "r") as t:
             while True:
                 linia = t.readline().strip()
+                self.baza.append(linia)
+                if linia == "":
 
-                if linia == "saldo":
 
-                    saldo = int(t.readline().strip())
-                    komentarz = str(input())
-                    if not self.saldo(saldo, komentarz):
-                        break
-        return self.magazyn
+                 break
+            return self.baza
+
+    def odczyt(self):
+
+            lista = self.lista()
+            for i in len(lista):
+                if(lista[i])=="zakup":
+                    lista[i+2]=int(lista[i+2])
+                    if lista[i+1] in self.magazyn:
+                        self.magazyn[lista[i+1]]+=lista[i+2]
+                        self.calkowite += lista[i + 2]
+                    else:
+                        self.magazyn[lista[i+1]]=lista[i+2]
+
+                return self.magazyn
+            for i in len(lista):
+                if(lista[i])=="sprzedarz":
+                    lista[i+2]=int(lista[i+2])
+                    if lista[i+1] in self.magazyn:
+                        self.magazyn[lista[i+1]]-=lista[i+2]
+
+                        return self.magazyn
+                    else:
+                     return False
+
+
+
+
+
+
+
 
 class Fundusz:
     def __init__(self, calkowite):
         self.calkowite=calkowite
 
 
-    def odczyt(self ):
-        with open("statussalda.txt", "r") as d:
-            self.calkowite = d.read()
-        self.calkowite = int(self.calkowite)
-        return self.calkowite
+
     def wpis(self):
         self.calkowite = str(self.calkowite)
         with open("statussalda.txt", "w") as plik1:
