65a1f0f6fcff87f7b22d622442689a186968b569
Xawery <ksawerynw@gmail.com>
2022-02-26 00:30:25 +0100

    pogoda ver1

--- lab.py
+++ lab.py
@@ -1,8 +1,5 @@
 import requests
 import sys
-
-print("podaj date")
-date=input()
 url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly"
 
 querystring = {"lat":"52.22","lon":"21.01"}
@@ -12,21 +9,76 @@ headers = {
     'x-rapidapi-key': key
     }
 
+class Weather:
+    def __init__(self):
+        self.test=""
+        self.baza=[]
+        self.lines=0
 
-with open("test.txt", "r") as file:
-    if file.readline() == "":
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        with open("test.txt", "w") as file:
-            file.write(response.text)
-            print("nadpis")
-    else:
+
+    def list(self):
+        base=self.baza
+
+        with open("logi.txt", "r") as file:
+            for line in file:
+
+                line=line.strip()
+                if line == "\n":
+                    continue
+                print(line)
+                base.append(line)
+            print(base)
+        return base
+    def check(self, date):
+        baza=self.list()
+        for i in range(len(baza)):
+            if baza[i] == date:
+               return True
+
+
+
+    def check2(self, response):
         with open("test.txt", "r") as file:
-            response=file.readline()
+            self.test = file.readline()
+        if self.test!=response:
+            with open("test.txt", "w") as file:
+                file.write(response.text)
+            return False
+    def action(self, response, date):
+        data=response.json()
+        for i in range(len(data["data"])):
+            if date in data["data"][i]["timestamp_local"]:
+                if "snow" or "rain" in data["data"][i]["weather"]["description"]:
+                    with open("logi.txt", "a") as fp:
+                        self.baza.append(date)
+                        for i in range(len(self.baza)):
+                            fp.write(self.baza[i])
+                            fp.write("\n")
 
-data=response.json()
-for i in range(len(data["data"])):
-    if date in data["data"][i]["timestamp_local"]:
-        if "snow" or "rain" in data["data"][i]["weather"]["description"]:
-            print("bedzie padac")
+                    print("bedzie padac")
 
-        break
+                break
+print("podaj date")
+date=str(input())
+sc=Weather()
+baza=sc.list
+print(date, "c1")
+if sc.check(date):
+    print("wczytane")
+    print("bedzie padac")
+    quit()
+
+else:
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    sc.check2(response)
+    sc.action(response, date)
+
+
+
+
+
+
+
+
+
+
