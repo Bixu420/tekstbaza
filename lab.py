cbb1ce29b7be28f474e770de9f0aec40a219c534
Xawery <ksawerynw@gmail.com>
2022-03-16 18:12:40 +0100

    pogoda2

--- lab.py
+++ lab.py
@@ -1,6 +1,12 @@
 import requests
 import sys
-url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly"
+import datetime
+from datetime import date
+
+today = datetime.date.today()
+
+
+url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily"
 
 querystring = {"lat":"52.22","lon":"21.01"}
 key=sys.argv[1]
@@ -30,13 +36,26 @@ class Weather:
                 base[line]=line1
             print(base)
         return base
-    def check(self, date, base):
+    def check(self, date1, base):
         baza=[]
-        if date in base.keys():
-            baza = [date, base[date]]
+        if date1 in base.keys():
+            baza = [date1, base[date1]]
             return baza
         else:
             return False
+    def check3(self, date1, today):
+        date1=date1.split("-")
+        year=int(date1[0])
+        month=int(date1[1])
+        today1=int(date1[2])
+        d0=date(year, month, today1)
+        delta=d0-today
+        if delta.days>16:
+            return False
+        return True
+
+
+
 
 
 
@@ -50,17 +69,18 @@ class Weather:
             with open("test.txt", "w") as file:
                 file.write(response.text)
             return False
-    def action(self, response, date, base):
+    def action(self, response, date1, base):
         data=response.json()
+
         for i in range(len(data["data"])):
             ciag=data["data"][i]["weather"]["description"]
-            if date in data["data"][i]["timestamp_local"]:
+            if date1 in data["data"][i]["timestamp_local"]:
                 if "snow" in ciag or "rain" in ciag:
 
                     with open("logi.txt", "a") as fp:
-                        base[date]="bedzie padac"
+                        base[date1]="bedzie padac"
                         fp.write("\n")
-                        fp.write(date)
+                        fp.write(date1)
                         fp.write("\n")
                         fp.write("bedzie padac")
 
@@ -69,9 +89,9 @@ class Weather:
                     break
                 else:
                     with open("logi.txt", "a") as fp:
-                        base[date]="nie bedzie padac"
+                        base[date1]="nie bedzie padac"
                         fp.write("\n")
-                        fp.write(date)
+                        fp.write(date1)
                         fp.write("\n")
                         fp.write("nie bedzie padac")
                     print("nie bedzie padac")
@@ -85,17 +105,24 @@ class Weather:
     def items2(self, base):
         return base.items()
 
+print("podaj date1")
+if len(sys.argv)==3:
+    date1=sys.argv[2]
+else:
 
-print("podaj date")
-date=str(input())
+    date1 = str(today + datetime.timedelta(days=1))
+
 sc=Weather()
 base=sc.list()
-print(date, "c1")
-check=sc.check(date, base)
+print(date1, "c1")
+if sc.check3(date1, today) is False:
+    print("nie wiem")
+    quit()
+check=sc.check(date1, base)
 if check is False:
     response = requests.request("GET", url, headers=headers, params=querystring)
     sc.check2(response)
-    lol=sc.action(response, date, base)
+    lol=sc.action(response, date1, base)
     for x, y in sc.items(lol):
         print(x, y)
 
